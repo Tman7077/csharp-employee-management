cf731f596e6a3c8b5bf4ebebaead84d1ce1e2668
Tyler Bartle <tman.bartleboy1@gmail.com>
2024-05-21 18:42:09 -0600

    I don't know why this was modified? :)

--- obj/Debug/net8.0/csharp-employee-management.AssemblyInfo.cs
+++ obj/Debug/net8.0/csharp-employee-management.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp-employee-management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d04f7c689d4eca7e9b4a1c52eb56d4f763fcb95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c0826ce6151e0393176f968cf91f9831dee3312")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp-employee-management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp-employee-management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
